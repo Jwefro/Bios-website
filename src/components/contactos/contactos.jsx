2dd2b9e5339f53a1bf4bd37f0c3012aecc637062
arturoluna879 <arturoluna879@gmail.com>
2021-10-22 22:40:20 -0400

    icons contact

--- src/components/contactos/contactos.jsx
+++ src/components/contactos/contactos.jsx
@@ -2,34 +2,49 @@ import './contactos.css'
 import React from 'react'
 import Footer from '../footer/footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faAddressBook, faAt, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {faAddressBook, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
+import {faWhatsapp, faInstagram} from '@fortawesome/free-brands-svg-icons';
 function Contactos() {
     return (
         <div>
 
             <div className="fondoContacto">
-            <div className="conta"><h1>Contactanos</h1><span></span></div>
-            
+                <div className="conta">
+                    <h1>Contactanos</h1>
+                    <a target="_blank"
+                       href="https://wa.me/584126405345?text=Hola,%20me%20gustaria%20contactarte%20estoy%20interesado%20en%20tu%20trabajo">
+                        <FontAwesomeIcon icon={faWhatsapp} className="cursor-pointer" />
+                    </a>
+                    <a target="_blank"
+                       href="https://instagram.com/bios.sc?utm_medium=copy_link">
+                        <FontAwesomeIcon icon={faInstagram} className="cursor-pointer" />
+                    </a>
+                    <a target="_blank"
+                       href="mailto:arturoluna879@gmail.com?Subject=Hola,%20me%20gustaria%20contactarte%20estoy%20interesado%20en%20tus%20servicios">
+                        <FontAwesomeIcon icon={faEnvelope} className="cursor-pointer" />
+                    </a>
+                </div>
+
 
             </div>
             <div className="separador">
-                    <p> Nuestro trato, rapidez, experiencia y el desarrollo de soluciones personalizadas. Nuestros clientes siempre son prioridad </p>
-                 </div>   
+                <p> Nuestro trato, rapidez, experiencia y el desarrollo de soluciones personalizadas. Nuestros clientes siempre son prioridad </p>
+            </div>
 
             <div className="container">
 
                 <h1 className="aten">Atencion al Cliente</h1>
 
-                <div className="infoContacto ">
+                <div className="infoContacto">
                     <div className="infoContactar">
-                        
+
                     <p> Puedes contactar con nosotros mediante el Email o el Telefono, y te daremos una respuesta lo antes posible:</p>
-                    
+
                     <div className="row">
                         <div className="col-1"><FontAwesomeIcon icon={faEnvelope} className="font" /></div>
                         <div className="col-3"> <span>  Email</span>
-                     
-                        
+
+
                          </div>   <h4 className="info">info@misitio.com</h4>
                          <hr/>
                     </div>
@@ -37,11 +52,9 @@ function Contactos() {
                         <div className="col-1"><FontAwesomeIcon icon={faPhone} className="font" /></div>
                         <div className="col-3"> <span> Telefono</span> </div>
                         <h4 className="info">914-123456</h4>
-                        
-                        
                          <hr/>
                     </div>
-                   
+
 
 
                     </div>
@@ -51,24 +64,24 @@ function Contactos() {
 
                         <input type="text" className="mb-4" placeholder="   Numero de Orden Ejemplo: 567"/>
                         <hr/>
-                        
+
                         <p>Si olvidaste tu numero de orden ingresa tu CI y te mostraemos tus ordenes registradas</p>
                         <input type="text" className="mb-4" placeholder="   Ingresa tu Ci Ejemplo: 27987564"/>
                     </div>
-                    
+
                 </div>
 
-               
+
             </div>
 
-            <h1 className="aten mb-5">¿Tienes Alguna consulta?</h1> 
+            <h1 className="aten mb-5">¿Tienes Alguna consulta?</h1>
  <div className="contac shadow ">
-                    
+
 
                 <div className="imgCon">
                     <div className="foncolsulta"><FontAwesomeIcon icon={faAddressBook} className="fontconsulta" />
                 <h4>INFORMACION DE CONTACTO</h4></div>
-                
+
 
                 </div>
 
@@ -95,9 +108,7 @@ function Contactos() {
 
                     </div>
                 </div>
-
             </div>
-           
            <Footer />
         </div>
     )
