7fdd1cd21e1bad28e8cf83fb7812fe29f53223d9
arturoluna879 <arturoluna879@gmail.com>
2021-11-10 00:01:20 -0400

    merge jeff

--- src/components/productos/productos.jsx
+++ src/components/productos/productos.jsx
@@ -71,13 +71,10 @@ function productos() {
             </div>
             <div className="sandiskConten">
                 <img src={pngegg} className="logoSandisk" alt="" />
-              
-                    <table class="tabla-datos"> <thead> <tr><th> </th>  </tr></thead> <tbody><tr> <td> <p>Capacidad</p> </td> <td> <p>1 TB (también de 2 TB, 250 GB y 500 GB)</p> </td> </tr> <tr> <td> <p>Tecnología</p> </td> <td> <p>Sandisk 3D TLC 64 capas</p> </td> </tr> <tr> <td> <p>Formatos</p> </td> <td> <p>Solo en unidad de 2,5 pulgadas</p> </td> </tr> <tr> <td> <p>Lectura secuencial</p> </td> <td> <p>560 MB/s</p> </td> </tr> <tr> <td> <p>Escritura secuencial</p> </td> <td> <p>530 MB/s</p> </td> </tr> <tr> <td> <p>Lectura aleatoria (IOPS)</p> </td> <td> <p>95K</p> </td> </tr> <tr> <td> <p>Escritura aleatoria (IOPS)</p> </td> <td> <p>84K</p> </td> </tr> <tr> <td> <p>TBW (TB)</p> </td> <td> <p>400</p> </td> </tr> <tr> <td> <p>Controlador</p> </td> <td> <p>Marvell 88SS1074</p> </td> </tr>  </tbody></table>
+
+                <table class="tabla-datos"> <thead> <tr><th> </th>  </tr></thead> <tbody><tr> <td> <p>Capacidad</p> </td> <td> <p>1 TB (también de 2 TB, 250 GB y 500 GB)</p> </td> </tr> <tr> <td> <p>Tecnología</p> </td> <td> <p>Sandisk 3D TLC 64 capas</p> </td> </tr> <tr> <td> <p>Formatos</p> </td> <td> <p>Solo en unidad de 2,5 pulgadas</p> </td> </tr> <tr> <td> <p>Lectura secuencial</p> </td> <td> <p>560 MB/s</p> </td> </tr> <tr> <td> <p>Escritura secuencial</p> </td> <td> <p>530 MB/s</p> </td> </tr> <tr> <td> <p>Lectura aleatoria (IOPS)</p> </td> <td> <p>95K</p> </td> </tr> <tr> <td> <p>Escritura aleatoria (IOPS)</p> </td> <td> <p>84K</p> </td> </tr> <tr> <td> <p>TBW (TB)</p> </td> <td> <p>400</p> </td> </tr> <tr> <td> <p>Controlador</p> </td> <td> <p>Marvell 88SS1074</p> </td> </tr>  </tbody></table>
             </div>
         </div>
-
-
-
             <Footer />
         </div>
     )
